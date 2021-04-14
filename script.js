56cc541f582dc61940adc890f8e45d1801f3dd53
Thiago Magalhaes <thiago_magalhaes@id.uff.br>
2021-04-14 09:43:57 -0300

    :package:menu sidebar

---
+++ script.js
@@ -0,0 +1,5 @@
+document.querySelector(".hamburguer").addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu") /* toggle = adiciona se não tiver a classe e remove se tiver a classe */
+);
+
+
